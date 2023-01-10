e6a5d9f8abca80a377d8e5dcfd6c2eceb227c393
Zach <ztartera5@gmail.com>
2023-01-09 23:48:18 -0500

    Added a color picker, and updated footer; PROJECT COMPLETE!

--- index.js
+++ index.js
@@ -4,33 +4,56 @@ const etchASketch = document.querySelector('.etch-a-sketch')
 function resizeGrid(width, height){
   const rows = document.querySelectorAll(".row");
   rows.forEach((row) => row.remove());
-
+  
   for (let i = 0; i < width; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     
     for (let j = 0; j < height; j++) {
       const tile = document.createElement("div");
+      tile.classList.add('tile')
       row.appendChild(tile);
-      tile.addEventListener('mouseover', () =>{
-        tile.style.backgroundColor = "black"
-      })
+    
       const resetButton = document.querySelector('.reset-button')
       resetButton.addEventListener('click', () =>{
         etchASketch.classList.add("shake")
         setTimeout(() => {
-          if(tile.style.backgroundColor == "black"){
+          if(tile.style.backgroundColor !== ""){
             tile.style.animation = "fadeOut 1s ease", 400
           }
           setTimeout(() => {tile.style.animation = ""}, 500)
         })
+
         setTimeout(() => {tile.style.backgroundColor = "white"}, 500)
-        setTimeout(() => { etchASketch.classList.remove('shake') }, 1000);
+        setTimeout(() => {etchASketch.classList.remove('shake') }, 1000);
       })
     }
     grid.appendChild(row);
     
   }
+}
+const drawQueue = document.createElement('div')
+drawQueue.style.fontFamily = "Lato"
+drawQueue.style.marginBottom = "5px"
+drawQueue.textContent = "Select a color, and start creating!"
+document.body.prepend(drawQueue)
+function selectColor(color) {
+
+  const tiles = document.querySelectorAll('.tile');
+
+  if (color == "psychadelic"){
+    tiles.forEach((tile) => tile.addEventListener('mouseover', () => {
+      tile.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+    }));
+    drawQueue.remove()
+  }  
+  else{
+    drawQueue.remove()
+    tiles.forEach((tile) => tile.addEventListener('mouseover', () => {
+      tile.style.backgroundColor = color;
+    }));
+  }
+
 }
 resizeGrid(32, 24)
 
